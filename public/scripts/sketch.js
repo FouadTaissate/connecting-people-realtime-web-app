f17285a776a448237d49a4d9f2fd0bdeef665482
youssra <112857270+zombie0youssra@users.noreply.github.com>
2023-06-01 09:42:54 +0200

    its done

--- public/scripts/sketch.js
+++ public/scripts/sketch.js
@@ -3,13 +3,15 @@ var socket;
 // canvas setup
 function setup() {
   createCanvas(1850, 750);
-  // background(0, 100, 200);
+  background(235, 235, 235);
 }
 
 function mouseDragged() {
   console.log(mouseX + "," + mouseY);
+
+  noStroke();
   fill(232, 182, 0);
-  ellipse(mouseX, mouseY, 30, 30);
+  ellipse(mouseX, mouseY, 25, 25);
 }
 
 // drawing function
